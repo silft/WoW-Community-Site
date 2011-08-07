f55ea644defdd4da4db24b7e40aec17e8610eb35
Shadez <d.master.me@gmail.com>
2011-08-08 02:03:19 +0900

    [195] Add WoW_Cacher class.

--- includes/revision_nr.php
+++ includes/revision_nr.php
@@ -1,6 +1,6 @@
 <?php
-define('WOW_REVISION', 194);
+define('WOW_REVISION', 195);
 define('DB_VERSION', 182);
 define('RUNONCE_REVISION', 73);
-define('CONFIG_VERSION', '0508201101');
+define('CONFIG_VERSION', '0808201101');
 ?>
